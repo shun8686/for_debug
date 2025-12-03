ba8ab173db1bd6a7f46e34920481b5bca52b8dd5
shun8686 <verve@vip.qq.com>
2025-12-03 20:34:52 +0800

    modify

--- scripts/get_net_interface.py
+++ scripts/get_net_interface.py
@@ -1,27 +1,22 @@
-import ipaddress
 import netifaces
+import socket
 
 def find_interface_by_ip(ip_address):
-    try:
-        ip = ipaddress.ip_address(ip_address)
-    except ValueError:
-        print("Invalid IP address")
-        return None
+    ip_binary = socket.inet_pton(socket.AF_INET, ip_address)
     
-    interfaces = netifaces.interfaces()
-    for interface in interfaces:
-        try:
-            addrs = netifaces.ifaddresses(interface)
-            for family in (netifaces.AF_INET, netifaces.AF_INET6):
-                if family in addrs:
-                    for addr in addrs[family]:
-                        if ip == ipaddress.ip_address(addr['addr']):
-                            return interface
+    for interface in netifaces.interfaces():
+        addresses = netifaces.ifaddresses(interface)
+        if netifaces.AF_INET in addresses:
+            for addr in addresses[netifaces.AF_INET]:
+
+                iface_ip_binary = socket.inet_pton(socket.AF_INET, addr['addr'])
+                if ip_binary == iface_ip_binary:
+                    return interface
     return None
 
-ip_address = "192.168.1.100"
+ip_address = "192.168.0.102"
 interface_name = find_interface_by_ip(ip_address)
 if interface_name:
-    print(f"Interface name for IP {ip_address} is: {interface_name}")
+    print(f"The interface for IP {ip_address} is: {interface_name}")
 else:
     print(f"No interface found for IP {ip_address}")
