5e99797843e67903a33cdfcaf5a9d37a227853ef
shun8686 <verve@vip.qq.com>
2025-12-04 13:00:02 +0800

    modify

--- scripts/get_net_interface.py
+++ scripts/get_net_interface.py
@@ -1,33 +1,26 @@
-import netifaces
+import ipaddress
+import psutil
 import socket
 
-def find_interface_by_ip(ip_address):
-    ip_binary = socket.inet_pton(socket.AF_INET, ip_address)
-    
-    for interface in netifaces.interfaces():
-        addresses = netifaces.ifaddresses(interface)
-        if netifaces.AF_INET in addresses:
-            for addr in addresses[netifaces.AF_INET]:
+def get_nic_name():
+    net_io_stats = psutil.net_if_addrs()
+    interfaces_with_192_ips = {}
 
-                iface_ip_binary = socket.inet_pton(socket.AF_INET, addr['addr'])
-                if ip_binary == iface_ip_binary:
-                    return interface
-    return None
+    for interface, addrs in net_io_stats.items():
+        for addr in addrs:
+            if addr.family == socket.AF_INET:
+                try:
+                    ip = ipaddress.ip_address(addr.address)
+                except ValueError:
+                    continue
+                if ip.is_private and ip.network.network_address == ipaddress.IPv4Network('192.0.0.0/8').network_address:
+                    interfaces_with_192_ips[interface] = addr.address
 
-def get_host_ip():
-    for interface in netifaces.interfaces():
-        addr = netifaces.ifaddresses(interface)
-        if netifaces.AF_INET in addr:
-            print(addr[netifaces.AF_INET][0]['addr'])
+    return interfaces_with_192_ips
 
-ip_address = "192.168.0.102"
-interface_name = find_interface_by_ip(ip_address)
-if interface_name:
-    print(f"The interface for IP {ip_address} is: {interface_name}")
-else:
-    print(f"No interface found for IP {ip_address}")
-
-
+interfaces = get_nic_name()
+for interface, ip in interfaces.items():
+    print(f"Interface: {interface}, IP Address: {ip}")
 
 
 
