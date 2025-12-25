f49b7171674437f1bed1f4619b1a9a1fd815e477
shun8686 <verve@vip.qq.com>
2025-12-25 21:40:48 +0800

    add

---
+++ multi_node_test/start.sh
@@ -0,0 +1,6 @@
+sglang_source_path=/data/d00662834/for_debug/
+test_case=multi_node_test/test_case_001.py
+node_size=2
+image=swr.cn-southwest-2.myhuaweicloud.com/base_image/dockerhub/lmsysorg/sglang:cann8.3.rc2-910b-release1225
+
+bash run_multi_node_test.sh $sglang_source_path $test_case $node_size $image
