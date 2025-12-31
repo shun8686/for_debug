e403413803fa379de6b54b9941ec634903ce8a24
shun8686 <verve@vip.qq.com>
2025-12-31 17:52:32 +0800

    add

---
+++ multi_node_test/deepseek/run_multi_node_test.sh
@@ -0,0 +1,58 @@
+sglang_source_path=$1
+test_case=$2
+node_size=$3
+image=$4
+
+if [ "$#" -lt 3 ];then
+  echo "Param num is less than 3. Exit."
+  exit 1
+fi
+
+if [ ! -f ${sglang_source_path}/${test_case} ]; then
+  echo "Testcase file is not exist: ${sglang_source_path}/${test_case}"
+  exit 1
+fi
+
+export KUBECONFIG=/data/.cache/kb.yaml
+export NAMESPACE=sglang-kernel-npu
+export KUBE_JOB_NAME=sglang-npu-multi
+export KUBE_JOB_TYPE=multi
+export KUBE_CONFIG_MAP=sglang-info
+export KUBE_YAML_FILE=k8s_multi.yaml
+echo "KUBE_JOB_NAME: $KUBE_JOB_NAME"
+SCRIPT_PATH=$(dirname $(readlink -f $0))
+
+# Clear resources
+kubectl delete -f ${SCRIPT_PATH}/${KUBE_YAML_FILE} --ignore-not-found=true || true
+
+pod_name_prefix="${KUBE_JOB_NAME}-sglang"
+echo "kube name space: $NAMESPACE, pod name prefix: ${pod_name_prefix}"
+while true; do
+  if kubectl get po -A -n $NAMESPACE | grep -q "${pod_name_prefix}"; then
+    echo "Found exist sglang job, sleeping for 30 seconds..."
+    sleep 30
+    kubectl get pods | grep "${pod_name_prefix}" | awk '{print $1}' | xargs kubectl delete pod -n $NAMESPACE || true
+  else
+    echo "No sglang job exist, start test case..."
+    break
+  fi
+done
+
+pip3 install kubernetes
+pip3 install -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple jinja2-cli
+
+echo "{ \"image\": $image,\
+	\"name_space\": \"$NAMESPACE\",\
+	\"kube_job_name\": \"$KUBE_JOB_NAME\",\
+	\"kube_config\": \"$KUBECONFIG\",\
+	\"kube_config_map\": \"$KUBE_CONFIG_MAP\",\
+	\"node_size\": $node_size,\
+	\"sglang_source_path\": \"$sglang_source_path\",\
+	\"test_case\": \"$test_case\" }"|\
+    jinja2 ${SCRIPT_PATH}/k8s_multi.yaml.jinja2 -o ${SCRIPT_PATH}/${KUBE_YAML_FILE}
+
+cd ${SCRIPT_PATH}
+python3 -u run_ascend_ci.py
+
+kubectl delete -f ${SCRIPT_PATH}/${KUBE_YAML_FILE}
+rm -rf ${SCRIPT_PATH}/${KUBE_YAML_FILE}
