c7f3a2466a1e6d2738a9a6de1fe45a5839495dff
shun8686 <verve@vip.qq.com>
2026-01-05 11:05:04 +0800

    update

---
+++ scripts/login_docker.sh
@@ -0,0 +1,1 @@
+docker exec -ti -uroot sgl-dingshun /bin/bash
---
+++ scripts/run_docker.sh
@@ -0,0 +1,32 @@
+docker run -itd --privileged --network=host --shm-size=16g \
+--name sgl-dingshun \
+-v /mnt:/mnt \
+-v /home:/home \
+-v /data/ascend-ci-share-pkking-sglang:/root/.cache \
+-v /data:/data \
+-v /usr/local/sbin:/usr/local/sbin \
+-v /usr/local/Ascend/driver:/usr/local/Ascend/driver \
+-v /usr/local/Ascend/firmware:/usr/local/Ascend/firmware \
+-v /etc/asceng_install.info:/etc/asceng_install.info \
+-v /var/queue_scheduler:/var/queue_scheduler \
+--device=/dev/davinci0 \
+--device=/dev/davinci1 \
+--device=/dev/davinci2 \
+--device=/dev/davinci3 \
+--device=/dev/davinci4 \
+--device=/dev/davinci5 \
+--device=/dev/davinci6 \
+--device=/dev/davinci7 \
+--device=/dev/davinci8 \
+--device=/dev/davinci9 \
+--device=/dev/davinci10 \
+--device=/dev/davinci11 \
+--device=/dev/davinci12 \
+--device=/dev/davinci13 \
+--device=/dev/davinci14 \
+--device=/dev/davinci15 \
+--device=/dev/davinci_manager \
+--device=/dev/hisi_hdc \
+--entrypoint=bash \
+--env HF_ENDPOINT=https://hf-mirror.com \
+swr.cn-southwest-2.myhuaweicloud.com/base_image/dockerhub/lmsysorg/sglang:cann8.3.rc2-a3-release20260105
