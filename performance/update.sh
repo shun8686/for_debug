3f29de9fd38b513e5f8dc1582989f30ec407425f
shun8686 <verve@vip.qq.com>
2026-01-07 09:26:38 +0800

    update

--- performance/update.sh
+++ performance/update.sh
@@ -1,4 +1,4 @@
 # pip install pymysql
 
-python insert_performance_data.py --log_dir ./metrics --host 127.0.0.1 --user test --password Huawei@123 --database performance_db
+python3 insert_performance_data.py --log_dir ./metrics --host 127.0.0.1 --user test --password Huawei@123 --database performance_db
 
